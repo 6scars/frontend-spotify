9835a5f3b67f4902e0e14a256cd795f4f39d4434
6scars <zasalatol@o2.pl>
2025-10-17 16:37:07 +0200

    change hierarhy of files

--- mini-spotify-frontend/src/Center/Music/Songs/Songs.jsx
+++ mini-spotify-frontend/src/Center/Music/Songs/Songs.jsx
@@ -1,4 +1,8 @@
+import {useState} from 'react'
 export default function Songs({ songs, authors, chooseSong }) {
+  const [loaded, setLoaded] = useState(false)
+
+  
   return (
     <div className="songs-container red-scroll-bar h-full">
       {songs.map((song) => {
@@ -16,7 +20,9 @@ export default function Songs({ songs, authors, chooseSong }) {
             <div className="song-container-inner  rounded-xl ">
               <div className="song-image-container h-[75%] ">
                 <img
-                  className="song__image"
+                  className={`song__image ${loaded ? "loaded" : "notLoaded"}`}
+                  onLoad={() => setLoaded(true)}
+                  onError={()=> console.error("Failed to load image")}
                   src={`https://rgmmwhkixprkskznqjcy.supabase.co/storage/v1/object/public/spotify/images/songPictures/${song.songImage}`}
                 ></img>
               </div>
