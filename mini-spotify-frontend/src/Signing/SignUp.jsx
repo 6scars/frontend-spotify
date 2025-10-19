e94b5b33fe88b945782ac36286f53e88425f958f
6scars <zasalatol@o2.pl>
2025-10-19 12:14:12 +0200

    swithc form if you don't have account

---
+++ mini-spotify-frontend/src/Signing/SignUp.jsx
@@ -0,0 +1,60 @@
+export default function SignUp({ signForm, setSwitchForm, clickedAccount}) {
+
+    return (
+        (<>
+
+            <div className="fixed inset-0 bg-black opacity-50 flex items-center justify-center z-[20]" />
+            <div className="flex w-[100%] h-[100%]">
+                <div className="fixed left-0 top-0 right-0 bottom-0  bg-transparent rounded-2xl shadow-lg p-8 w-full  z-[25]
+                        flex justify-center items-center
+                    ">
+                    <div className="Sign-container w-[50%] h-[50%] bg-[var(--background-color)] rounded-xl
+                            text-white
+                        ">
+                        <div className="Sign_in_text_wrapper h-[20%]">
+                            <h3 className="sign_in  ">Register up</h3>
+                        </div>
+
+                        <div className="devider" />
+                        <div className="Sign-form-Wrapper w-[100%] h-[79%] align-middle flex justify-center items-center">
+                            <form className="flex flex-col gap-5">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email"
+                                    className="form__input "
+                                    onChange={() => signForm(event)}
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    className="form__input "
+                                    onChange={() => signForm(event)}
+                                />
+                                <button
+                                    type="submit"
+                                    className="form__button "
+                                >
+                                    Register
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={clickedAccount}
+                                    className="form__button__cancel "
+                                >
+                                    Cancel
+                                </button>
+                                <div>
+                                    Don't you have account? <a onClick={() =>  setSwitchForm(false)} className="text-blue-500 cursor-pointer hover:text-amber-500">LoginIn</a>
+
+                                </div>
+                            </form>
+                        </div >
+                    </div>
+                </div>
+            </div>
+
+        </>)
+    )
+}
--- mini-spotify-frontend/src/Signing/Signing.jsx
+++ mini-spotify-frontend/src/Signing/Signing.jsx
@@ -1,77 +1,30 @@
 import { useState } from 'react'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 import "./Signing.css"
 
 export default function Signing({ clickedAccount }) {
-    const [xd, setXd] = useState({
+    const [formValue, setFormValue] = useState({
         email: '',
         password: ''
     })
 
-    const signForm = (e) => {
-        console.log(e.target.name)
-        setXd((prev) => ({
-            ...prev,
-            [e.target.name] : e.target.value
-        }))
+    const [switchForm, setSwitchForm] = useState(false);
 
-        console.log(xd)
+    const signForm = (e) => {
+        setFormValue((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
     }
 
     return (
-        (
-            <>
+        <>
+            {
+                !switchForm ? <SignIn signForm={signForm} setSwitchForm={setSwitchForm} clickedAccount={clickedAccount} /> : <SignUp signForm={signForm} setSwitchForm={setSwitchForm} clickedAccount={clickedAccount} />
+            }
+        </>
 
-                <div className="fixed inset-0 bg-black opacity-50 flex items-center justify-center z-[20]" />
-                <div className="flex w-[100%] h-[100%]">
-                    <div className="fixed left-0 top-0 right-0 bottom-0  bg-transparent rounded-2xl shadow-lg p-8 w-full  z-[25]
-                        flex justify-center items-center
-                    ">
-                        <div className="Sign-container w-[50%] h-[50%] bg-[var(--background-color)] rounded-xl
-                            text-white
-                        ">
-                            <div className="Sign_in_text_wrapper h-[20%]">
-                                <h3 className="sign_in  ">Sign In</h3>
-                            </div>
 
-                            <div className="devider" />
-                            <div className="Sign-form-Wrapper w-[100%] h-[79%] align-middle flex justify-center items-center">
-                                <form className="flex flex-col gap-5">
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email"
-                                        className="form__input "
-                                        onChange={() => signForm(event)}
-                                    />
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        placeholder="Password"
-                                        className="form__input "
-                                        onChange={() => signForm(event)}
-                                    />
-                                    <button
-                                        type="submit"
-                                        className="form__button "
-                                    >
-                                        Sign In
-                                    </button>
-                                    <button
-                                        type="button"
-                                        onClick={clickedAccount}
-                                        className="form__button__cancel "
-                                    >
-                                        Cancel
-                                    </button>
-                                </form>
-                            </div >
-
-                        </div>
-
-                    </div>
-                </div>
-
-            </>
-        )
     )
 }
