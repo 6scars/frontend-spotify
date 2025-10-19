d9e71337c1aa653a3d946d1bcbb9d4567e70dabb
6scars <zasalatol@o2.pl>
2025-10-19 17:39:27 +0200

    create router and controller

--- mini-spotify-backend/server.js
+++ mini-spotify-backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import router from './router.js'
 
 
 const app = express();
@@ -8,19 +9,7 @@ const PORT = 3005;
 
 app.use(express.json());
 app.use(cors());
-app.post('/api/newAccount', (req, res) => {
-    res.status(201).json({
-        message: "accomplished"
-    })
-})
-app.post('/api/signin', (req, res) => {
-    res.status(201).json({
-        message: "accomplished"
-    })
-})
-app.get('/', (req, res) => {
-    res.send("hello");
-})
+app.use("/api",router)
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`)
