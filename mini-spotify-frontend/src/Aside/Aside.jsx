41e9775f3c6fa8b16628d92465880f726013a396
6scars <zasalatol@o2.pl>
2025-10-23 00:36:45 +0200

    create queries and swap aside data

--- mini-spotify-frontend/src/Aside/Aside.jsx
+++ mini-spotify-frontend/src/Aside/Aside.jsx
@@ -1,11 +1,10 @@
 import "./Aside.css";
-import Playlists from "./PlayLists";
-import playlists from "../../public/data/playlists.json";
-export default function Aside({ show, songs }) {
+import Playlists from './PlayLists'
+
+export default function Aside({ show, songs, playlists }) {
   const choosePlaylist = (playlistId) => {
     console.log(playlistId);
   };
-  console.log(playlists[1].songsId);
   return (
     <aside
       className={`leftBar red-scroll-bar fixed top-[89px] bottom-[0px] bg-gray-900 rounded-xl
@@ -13,14 +12,14 @@ export default function Aside({ show, songs }) {
             overflow-y-auto
             ${show ? "show" : ""}`}
     >
-      {playlists.map((playlist) => (
+       {playlists.map((playlist) => (
         <Playlists
           key={playlist.id}
           playlist={playlist}
           choosePlaylist={choosePlaylist}
           songs={songs}
         />
-      ))}
+      ))} 
     </aside>
   );
 }
