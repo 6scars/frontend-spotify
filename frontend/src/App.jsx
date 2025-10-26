396244059f7ea359b1fe072c21d2797074d6c0c2
6scars <zasalatol@o2.pl>
2025-10-26 19:40:30 +0100

    change name of files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import Header from "./Header/Header.jsx";
+import Aside from "./Aside/Aside.jsx";
+import Center from "./Center/Center.jsx";
+import Play from "./Play/Play.jsx";
+import Signing from "./Signing/Signing.jsx";
+import { checkToken, fetchPlaylists } from "./scripts/Fetches.jsx";
+import "./App.css";
+
+
+export default function MiniSpotify() {
+  const [currentSong, setCurrentSong] = useState(null);
+  const [show, setShow] = useState(false);
+  const [signing, setSigning] = useState(false)
+  const [playlists, setPlaylists] = useState([]);
+  const [isLogedIn, setIsLogedIn] = useState(false);
+
+  const user_id = Number(localStorage.getItem('user_id'))
+
+
+
+  useEffect(() => {
+    const fetches = async () => {
+      setIsLogedIn(await checkToken());
+      setPlaylists(await fetchPlaylists(user_id))
+    }
+    fetches()
+  }, []);
+
+
+
+
+  const clickedAccount = () => {
+    signing ? setSigning(false) : setSigning(true)
+  }
+
+  return (
+    <>
+
+      <Header
+        clickedAccount={clickedAccount}
+        isLogedIn={isLogedIn}
+        setIsLogedIn={setIsLogedIn}
+      />
+      <Center
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        show={show}
+        setShow={setShow}
+      />
+      {currentSong ? <Play currentSong={currentSong} /> : ""}
+      {playlists ? <Aside show={show} playlists={playlists} /> : null}
+      {signing && <Signing setIsLogedIn={setIsLogedIn} clickedAccount={clickedAccount} isLogedIn={isLogedIn} />}
+
+    </>
+  );
+}
---
+++ frontend/src/scripts/Fetches.jsx
@@ -0,0 +1,52 @@
+export async function fetchSongs() {
+    const response = await fetch('http://localhost:3005/api/fetchSongs');
+    const data = await response.json();
+    return data.data
+}
+
+export async function fetchPlaylists(id) {
+    try {
+        const playlistsResponse = await fetch('http://localhost:3005/api/playlists', {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(
+                { id }
+            )
+
+
+        });
+        const data = await playlistsResponse.json();
+        const d = data.data
+        return d
+
+    } catch (error) {
+        console.error('Error fetching dataPlaylists:', error);
+    }
+};
+
+export async function checkToken() {
+    try {
+        const dataUserResponse = await fetch('http://localhost:3005/api/checkToken', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await dataUserResponse.json();
+        if (data.token) {
+            return true
+        }
+        localStorage.removeItem('jwt');
+        localStorage.removeItem('user_id')
+        return false
+    } catch (error) {
+        console.error('Error fetching  dataUser:', error);
+        return false
+    }
+
+};
+
+
