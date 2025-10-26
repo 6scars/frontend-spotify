2e0376e1866a1a7290bb0f6c80658d7fb4d8a9a5
6scars <zasalatol@o2.pl>
2025-10-26 20:18:32 +0100

    fetch view ++ if someone clicked on song

--- backend/router.js
+++ backend/router.js
@@ -8,4 +8,5 @@ router.post('/signin', controller.signIn)
 router.post('/playlists', controller.playlists)
 router.post('/checkToken', controller.checkToken)
 router.get('/fetchSongs',controller.fetchSongs)
+router.get('/:id/addView',controller.addView)
 export default router;
