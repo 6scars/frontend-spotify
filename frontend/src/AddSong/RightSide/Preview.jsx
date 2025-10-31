34c54a2f6b42d06e5b928fa8013b605b0d5b3c43
6scars <zasalatol@o2.pl>
2025-10-31 21:49:58 +0100

    create preview add song button and fetch function api in frontend

--- frontend/src/AddSong/RightSide/Preview.jsx
+++ frontend/src/AddSong/RightSide/Preview.jsx
@@ -1,15 +1,34 @@
 import Image from './Information/Image'
 import ImportedMp3 from './File/ImportedMp3'
+import './Preview.css';
 export default function Preview({ addSongForm }) {
     return (
         <>
-            <div className=" flex flex-col items-center justify-center w-full h-full ">
+            <div className="Preview flex flex-col items-center justify-center w-full h-full ">
                 <div className="w-[50%] flex items-center justify-center">
                     <Image imgUrl={addSongForm.imgUrl} />
                 </div>
 
+                <div
+                    className="authors-container  h-[25%] text-gray-700 font-bold flex items-center flex flex-col" >
+                    <div className=" text-[0.8rem] text-[var(--main-color)]">
+                        {addSongForm.song_name || 'title'}
+                    </div>
+                    <p className="  text-[0.7rem] authors__text ">
+                        author_name
+                    </p>
+                    <p className="text-[0.7rem] text-white font-thin">
+                        {`${addSongForm.credit || 'credit'}`}
+                    </p>
+                </div>
+
                 <div>
-                    <ImportedMp3 addSongForm={addSongForm}/>
+                    <ImportedMp3 addSongForm={addSongForm} />
+                </div>
+
+                <div className="button-container">
+                    <button className="bg-white" >SEND SONG</button>
+
                 </div>
             </div>
         </>
