36d303bb1afd46d24ca01479d66fe05afdab57da
6scars <zasalatol@o2.pl>
2025-11-18 00:55:51 +0100

    make web site responsive 5

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -6,42 +6,36 @@ import Sing from './Sing'
 
 export default function Header() {
 
-  const {clickedAccount, setReloadApp} = useUIStateContext();
-  const {isLogedIn, setIsLogedIn} = useAuthContext();
+  const { clickedAccount, setReloadApp } = useUIStateContext();
+  const { isLogedIn, setIsLogedIn } = useAuthContext();
 
-  const handleHomeClick = ()=>{setReloadApp(prev => !prev)}
+  const handleHomeClick = () => { setReloadApp(prev => !prev) }
   return (
     <header
-      className="fixed top-0 flex justify-betwen
-        "
-    >
+      className="fixed top-0 flex justify-between">
       <div className="left-header">
         <div className="logo-container h-full w-[96px] flex justify-center items-center">
-          <img  alt="logo" src="https://rgmmwhkixprkskznqjcy.supabase.co/storage/v1/object/public/spotify/images/logos/logo1.svg" className="h-[90%] " />
+          <img alt="logo" src="https://rgmmwhkixprkskznqjcy.supabase.co/storage/v1/object/public/spotify/images/logos/logo1.svg" className="h-[90%] " />
         </div>
       </div>
       <div
-        className="mid-header bg-black  flex-1 h-[full]
-        flex justify-center
-      "
-      >
+        className="mid-header bg-black flex-1 h-[full] flex justify-center ">
         <div
-          className="home-search w-[500px]
-        flex align-center justify-center
+          className="home-search min-w-[200px] flex align-center justify-center
         "
         >
-          <div className="h-full flex justify-center items-center">
+          <div className="h-full min-w-[43.5px] flex justify-center items-center">
             <img onClick={handleHomeClick} alt="home" src="https://rgmmwhkixprkskznqjcy.supabase.co/storage/v1/object/public/spotify/images/logos/logohome.svg" className="h-[75%] cursor-pointer"></img>
           </div>
-          <div className="h-full flex justify-center items-center">
+          <div className="h-full min-w-[20px] flex justify-center items-center ">
             <input type="text" placeholder="Search" className="search__bar" />
           </div>
         </div>
       </div>
-      <div className="right-header  min-w-[300px] h-full">
-        <div className=" flex justify-end items-center h-full text-white">
+      <div className="right-header  min-w-[60px] h-full">
+        <div className=" flex justify-end tems-center h-full text-white">
 
-          <Sing clickedAccount={clickedAccount} isLogedIn={isLogedIn}/>
+          <Sing clickedAccount={clickedAccount} isLogedIn={isLogedIn} />
         </div>
       </div>
     </header>
