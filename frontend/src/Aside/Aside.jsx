10ca674d7a5a107313d7c28fe46f9b3838bddabf
6scars <zasalatol@o2.pl>
2025-11-17 17:17:48 +0100

    make web site responsie 2

--- frontend/src/Aside/Aside.jsx
+++ frontend/src/Aside/Aside.jsx
@@ -28,10 +28,7 @@ export default function Aside() {
   };
   return (
     <aside
-      className={`leftBar red-scroll-bar fixed top-[89px] bottom-[0px] bg-gray-900 rounded-xl
-            flex flex-col items-center gap-[10px] 
-            overflow-y-auto
-            ${show ? "show" : ""}`}
+      className={`leftBar  red-scroll-bar  ${show ? "show" : ""}`}
     >
       <div onClick={displayCreatingPlaylistWindow} className="text-nowrap text-[0.8rem] text-red-500 hover:text-white cursor-pointer">
         <span>create playlist</span>
