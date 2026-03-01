18b47a124e327d3b11a1f8d71ff5d78ebd38edc5
6scars <zasalatol@o2.pl>
2026-03-02 00:19:21 +0100

    Refactor/Feature(Clear code, errorHandler) - Make code clearer, add express errorHandler

--- backend/modules/helper-functions/createToken.js
+++ backend/modules/helper-functions/createToken.js
@@ -1,17 +1,18 @@
-import jwt from "jsonwebtoken"
+import jwt          from "jsonwebtoken"
+import AppError     from "../../config/errorHandler/errorHandler.js";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export default async function createToken(userId, userEmail){
     try{
         return await jwt.sign({
-                id: userId,
-                email: userEmail
+                id:     userId,
+                email:  userEmail
             },
                 JWT_SECRET,
                 { expiresIn: '1h' }
             )
     }catch(err){
-        throw ("createToken", err)
+       throw new AppError( err.message || "create token error", err.status || 500);
     }
 
 }
---
+++ backend/modules/helper-functions/jwtVerifyReturnPayload.js
@@ -0,0 +1,14 @@
+import jwt              from "jsonwebtoken"
+import AppError         from "../../config/errorHandler/errorHandler.js";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+export default function jwtVerifyReturnPayload(token){
+    try{
+        const payload = jwt.verify(token, JWT_SECRET);
+        return payload
+    }catch(err){
+        throw new AppError("Token is not valid or expired", 401);
+    }
+}
