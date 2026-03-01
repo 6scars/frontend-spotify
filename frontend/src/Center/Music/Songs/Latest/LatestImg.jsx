4465c1e092109c7eac654d8fe3e9ae9ede344e86
6scars <zasalatol@o2.pl>
2026-03-02 00:55:27 +0100

    Feature(shimmer) - add shimmer effect for latest song precisely the images

--- frontend/src/Center/Music/Songs/Latest/LatestImg.jsx
+++ frontend/src/Center/Music/Songs/Latest/LatestImg.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react"
+import                   "./latestImg.css"
 
 
 export default function LatestImg({img}){
-    const [src, setSrc] = useState(`${import.meta.env.VITE_SUPA_B_STOR}/images/songPictures/${img}`)
+    const [loaded, setLoaded]       = useState(false)
+    const [src, setSrc]             = useState(`${import.meta.env.VITE_SUPA_B_STOR}/images/songPictures/${img}`)
+
+    function render(){
+        return(
+            <div className="w-full h-full">
+                {!loaded && (
+                        <div className=" h-full w-full shimmer"></div>
+                    )}
+                <img
+                    className="playlist__image"
+                    onLoad={()=>setLoaded(true)}
+                    onError={()=>{setSrc(`${import.meta.env.VITE_SUPA_B_STOR}/images/NOTFOUND.jpeg`);}}
+                    src={src}
+                />
+            </div>
+        )
+    }
 
     return(
-        <img
-            className="playlist__image"
-            src={src}
-            onError={()=>{setSrc(`${import.meta.env.VITE_SUPA_B_STOR}/images/NOTFOUND.jpeg`);}}
-        ></img>
+    <>
+        {render()}
+    </>
     )
 }
