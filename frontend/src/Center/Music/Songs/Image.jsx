1943390277e9de8716b44b71bb13288b8b4fcd9d
6scars <zasalatol@o2.pl>
2026-03-02 01:12:47 +0100

    Refactor(shimmer) - move style to upper style(app.css)

--- frontend/src/Center/Music/Songs/Image.jsx
+++ frontend/src/Center/Music/Songs/Image.jsx
@@ -1,11 +1,10 @@
 import {useState}       from "react"
-import                       "./Image.css"
 
 export default function Image({ song_image }) {
     const [loaded, setLoaded]   = useState(false)
     const [src, setSrc]         = useState(`${import.meta.env.VITE_SUPA_B_STOR}/images/songPictures/${song_image}`)
     return (
-        <div className="song-image-container h-[75%] w-full">
+        <div className="song-image-container h-[75%] w-full rounded-xl">
             {!loaded && (
                 <div className="h-full w-full shimmer"> </div>
                 )}
