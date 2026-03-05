5a9b70bc49295d7477e8bff978641b0a5a3053f8
6scars <zasalatol@o2.pl>
2026-03-06 00:26:48 +0100

    BugFix(playlistDescription) - Crash application if playlist is empty. Eliminate volnerability of empty array

--- frontend/src/Center/PlaylistDescribing.jsx
+++ frontend/src/Center/PlaylistDescribing.jsx
@@ -1,6 +1,6 @@
 import { useEffect }                        from 'react'
 import SongInThePlaylist                    from "./PlaylistDescribing/SongInThePlaylist"
-import './PlaylistDescribing.css'
+import                                           './PlaylistDescribing.css'
 import { useCurrentPlaybackContext }        from '../contexts/CurrentPlaybackContext';
 import { useUIStateContext }                from '../contexts/UIStateContext';
 import usePlaylists                         from '../hooks/usePlaylists'
@@ -20,32 +20,8 @@ export default function PlaylistDescribing() {
     }, [playlist_id])
 
 
-
     const render = () => {
-        if (isLoading) {
-            return (
-                <div className="music red-scroll-bar space-y-4 bg-[#232323] flex-[2] h-full min-w-[500px] overflow-y-auto  rounded-md
-                    relative  flex justify-center items-center playlist-describing" >
-                    <div className="playlist-describing-wrapper w-[90%] h-[95%]  ">
-                        <div className="playlist-name-container flex justify-start items-center ">
-                            <span className="playlist_name_text">{}</span>
-                        </div>
-
-                        <div className="playlist-controlls flex justify-end items-center ">
-                            <img onClick={() => handleStartPlaylist()} alt="play button" className="h-12 cursor-pointer" src={`https://rgmmwhkixprkskznqjcy.supabase.co/storage/v1/object/public/spotify/images/logos/startSong.svg`} />
-                        </div>
-                        <div className="devider" />
-                        <div className="playlist-songs-container flex flex-col justify-center items-center w-full">
-                            {playlist.map((song) => (
-                                <SongInThePlaylist key={song.song_id} song={song} handleStartPlaylist={handleStartPlaylist} isLoading={isLoading}/>
-                            )
-                            )}
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-        if (!isLoading) {
+        if (!isLoading  && Array.isArray(playlist) && playlist.length > 0 ) {
             return (
                 <div className="music red-scroll-bar space-y-4 bg-[#232323] flex-[2] h-full min-w-[500px] overflow-y-auto  rounded-md
                     relative  flex justify-center items-center playlist-describing" >
