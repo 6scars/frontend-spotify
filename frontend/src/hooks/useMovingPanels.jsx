60f6625f62bf8e84cdb4194647c57d0fc524ad18
6scars <zasalatol@o2.pl>
2026-03-29 20:14:07 +0200

    BugFix(moving Panels) - there was not properly calculated width.

--- frontend/src/hooks/useMovingPanels.jsx
+++ frontend/src/hooks/useMovingPanels.jsx
@@ -1,7 +1,6 @@
 import { useState, useRef, useEffect } from "react"
-import { useUIStateContext } from "../contexts/UIStateContext"
 
-export function useMovingPanels (initial = 100){
+export function useMovingPanels (initial = 50){
     const [leftWidth, setLeftWidth]         = useState(initial)
     const isDragging                        = useRef(false) 
     const rectObject                        = useRef(null)
@@ -12,19 +11,9 @@ export function useMovingPanels (initial = 100){
             const rect              = rectObject.current.getBoundingClientRect();
             const deltaWidthPx      = e.clientX - rect.left;
             const deltaWidthPrc     = (deltaWidthPx / rect.width) * 100
-
-            console.log(rect)
-            console.log(rect.left)
-            console.log(e.clientX)
-            console.log(deltaWidthPx)
-            console.log(deltaWidthPrc)
-
             setLeftWidth(deltaWidthPrc)
-
-
             
         }
-
 
         const handleMouseUp = () => {
             isDragging.current = false;
@@ -43,7 +32,6 @@ export function useMovingPanels (initial = 100){
     function onMouseDown(e){
         console.log(e)
         isDragging.current = true;
-        console.log("isDragging.current ON", isDragging.current)
     }
 
     console.log("useMovingPanels")
