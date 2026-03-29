afa1ad6fbe1fe8a1ed8eec7591235b364dffe458
6scars <zasalatol@o2.pl>
2026-03-29 21:05:16 +0200

    Improve( imporve movingPanels) - change cursor when cursor is down  on resizer, add render throttling using requestAnimationFrame

--- frontend/src/hooks/useMovingPanels.jsx
+++ frontend/src/hooks/useMovingPanels.jsx
@@ -4,21 +4,32 @@ export function useMovingPanels (initial = 50){
     const [leftWidth, setLeftWidth]         = useState(initial)
     const isDragging                        = useRef(false) 
     const rectObject                        = useRef(null)
+    const frame                             = useRef(null) 
 
     useEffect(()=>{
         const handleMouseMove = (e) => {
-            if(!isDragging.current) return
-            const rect              = rectObject.current.getBoundingClientRect();
-            const deltaWidthPx      = e.clientX - rect.left;
-            const deltaWidthPrc     = (deltaWidthPx / rect.width) * 100
-            setLeftWidth(deltaWidthPrc)
-            
+            if(!isDragging.current || !rectObject.current)  return
+            if(frame.current )                              return
+
+            frame.current = requestAnimationFrame(()=>{
+                const rect                      = rectObject.current.getBoundingClientRect();
+                const deltaWidthPx              = e.clientX - rect.left;
+                const deltaWidthPrc             = (deltaWidthPx / rect.width) * 100
+
+                const clamped                   =  Math.min(90, Math.max(10, deltaWidthPrc))
+                setLeftWidth(clamped)    
+
+                frame.current = null
+            })
+
         }
 
         const handleMouseUp = () => {
             isDragging.current = false;
-            console.log("isDragging.current OFF", isDragging.current)
+            document.body.classList.remove("dragging")
+            document.body.style.cursor = "default"
         }
+        
         window.addEventListener("mousemove", handleMouseMove)
         window.addEventListener("mouseup", handleMouseUp)
         
@@ -29,12 +40,12 @@ export function useMovingPanels (initial = 50){
     },[])
 
     
-    function onMouseDown(e){
-        console.log(e)
+    function onMouseDown(){
         isDragging.current = true;
+        document.body.classList.add("dragging")
+        document.body.style.cursor = "col-resize"
     }
 
-    console.log("useMovingPanels")
     return{
         leftWidth,
         onMouseDown,
